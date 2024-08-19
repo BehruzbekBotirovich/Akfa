f80f5a8e9c3eb42d1c52d74d1c1dc5491d33f243
BehruzbekBotirovich <behruzbekbotirovich@gmail.com>
2024-08-19 10:56:34 +0500

    Customers slider added

--- src/components/OurCustomers.vue
+++ src/components/OurCustomers.vue
@@ -31,24 +31,19 @@
 <script setup>
 import { ref } from 'vue';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons-vue';
-const images = ref([
-    slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide10, slide11, slide12, slide13, slide14
-]);
+const images = ref([slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide10])
 
-import slide1 from '@/assets/slide_images/slide1.jpg';
-import slide2 from '@/assets/slide_images/slide2.jpg';
-import slide3 from '@/assets/slide_images/slide3.jpg';
-import slide4 from '@/assets/slide_images/slide4.jpg';
-import slide5 from '@/assets/slide_images/slide5.jpg';
-import slide6 from '@/assets/slide_images/slide6.jpg';
-import slide7 from '@/assets/slide_images/slide7.jpg';
-import slide8 from '@/assets/slide_images/slide8.jpg';
-import slide9 from '@/assets/slide_images/slide9.jpg';
-import slide10 from '@/assets/slide_images/slide10.jpg';
-import slide11 from '@/assets/slide_images/slide11.jpg';
-import slide12 from '@/assets/slide_images/slide12.jpg';
-import slide13 from '@/assets/slide_images/slide13.jpg';
-import slide14 from '@/assets/slide_images/slide14.jpg';
+import slide1 from '@/assets/images/client1.png';
+import slide2 from '@/assets/images/client1.png';
+import slide3 from '@/assets/images/client1.png';
+import slide4 from '@/assets/images/client1.png';
+import slide5 from '@/assets/images/client1.png';
+import slide6 from '@/assets/images/client1.png';
+import slide7 from '@/assets/images/client1.png';
+import slide8 from '@/assets/images/client1.png';
+import slide9 from '@/assets/images/client1.png';
+import slide10 from '@/assets/images/client1.png';
+
 
 const carousel = ref(null);
 
